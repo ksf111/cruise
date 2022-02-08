21aace360383dad491af2f453a0b81cd73aa3323
ksf1 <viacheslav.kisov@gmail.com>
2022-02-08 14:03:56 +0200

    08.02 #3

--- src/main/java/com/epam/command/LoginCommand.java
+++ src/main/java/com/epam/command/LoginCommand.java
@@ -42,11 +42,10 @@ public class LoginCommand implements Command {
 			e.printStackTrace();
 		}
 		StringBuffer sb = new StringBuffer();
-		for( int i=0; i<passwordBytes.length; i++ ) {
-			byte b = passwordBytes[ i ];
+		for(int i = 0; i < passwordBytes.length; i++) {
+			byte b = passwordBytes[i];
 			String hex = Integer.toHexString((int) 0x00FF & b);
-			if (hex.length() == 1)
-			{
+			if (hex.length() == 1){
 				sb.append("0");
 			}
 			sb.append(hex);
